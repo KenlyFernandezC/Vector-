b767bbee7f02f81029b67c35e631db3e6cbd60c3
KenlyFernandezC <fernandezbarbuis@gmail.com>
2024-02-12 13:03:34 -0600

    Intercambio

--- ListaDeNumeros.js
+++ ListaDeNumeros.js
@@ -8,7 +8,7 @@ while (huboIntercambio) {
         if (listaNumero[i] > listaNumero[i + 1]) {
 
             let Guardado = listaNumero[i];
-
+    
             listaNumero[i] = listaNumero[i + 1];
 
             listaNumero[i + 1] = Guardado;
@@ -19,4 +19,3 @@ while (huboIntercambio) {
 
 }
 console.log(listaNumero)
-
